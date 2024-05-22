f9bde2905686c5253ac484b6c3f44b9f8a49e6e4
Ihor Tokar <134220707+IhorTokar@users.noreply.github.com>
2024-05-23 00:38:06 +0300

    Update 5 Завдання.sql

--- 5 Завдання.sql
+++ 5 Завдання.sql
@@ -1,3 +1,20 @@
--- Зміна типу обмеження цілісності для зв'язку
-ALTER TABLE Recipes DROP FOREIGN KEY fk_user_id;
-ALTER TABLE Recipes ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE SET NULL;
+USE CulinaryForum;
+GO
+
+-- Вилучення старого зовнішнього ключа, якщо він існує
+-- У цьому випадку у нас немає специфічного імені для обмеження, тому використаємо загальне видалення обмеження
+ALTER TABLE Recipes DROP CONSTRAINT IF EXISTS fk_user_id;
+GO
+
+-- Зміна поля user_id, щоб дозволити значення NULL
+ALTER TABLE Recipes
+ALTER COLUMN user_id INT NULL;
+GO
+
+-- Додавання нового зовнішнього ключа з новим обмеженням
+ALTER TABLE Recipes
+ADD CONSTRAINT fk_user_id
+FOREIGN KEY (user_id)
+REFERENCES Users(user_id)
+ON DELETE SET NULL;
+GO
